72dfe3aec1e3ebef52dca77140b818acdaccd9c3
smfssilva <smfssilva@yahoo.com>
2021-09-25 16:02:15 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/smfssilva/cursomc/domain/PagamentoBoleto.java
+++ src/main/java/com/smfssilva/cursomc/domain/PagamentoBoleto.java
@@ -10,7 +10,7 @@ import com.smfssilva.cursomc.domain.enums.EstadoPagamento;
 @Entity
 public class PagamentoBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date dataVencimento;
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
