ff02fdce114841c9a036c5c179aad481b9a32748
smfssilva <smfssilva@yahoo.com>
2021-09-25 18:26:36 -0300

    Validação sintática com Bean Validation:

--- src/main/java/com/smfssilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/smfssilva/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.smfssilva.cursomc.domain.Categoria;
+import com.smfssilva.cursomc.dto.CategoriaDTO;
 import com.smfssilva.cursomc.repositories.CategoriaRepository;
 import com.smfssilva.cursomc.services.exceptions.DataIntegretyException;
 import com.smfssilva.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
