711c6756dad92ecfdcc036dd710ed1dd2cbb43a5
smfssilva <smfssilva@yahoo.com>
2021-09-25 03:18:20 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.smfssilva.cursomc.domain.Categoria;
 import com.smfssilva.cursomc.domain.Cidade;
+import com.smfssilva.cursomc.domain.Cliente;
+import com.smfssilva.cursomc.domain.Endereco;
 import com.smfssilva.cursomc.domain.Estado;
 import com.smfssilva.cursomc.domain.Produto;
+import com.smfssilva.cursomc.domain.enums.TipoCliente;
 import com.smfssilva.cursomc.repositories.CategoriaRepository;
 import com.smfssilva.cursomc.repositories.CidadeRepository;
+import com.smfssilva.cursomc.repositories.ClienteRepository;
+import com.smfssilva.cursomc.repositories.EnderecoRepository;
 import com.smfssilva.cursomc.repositories.EstadoRepository;
 import com.smfssilva.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcV2Application implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
@@ -60,6 +69,16 @@ public class CursomcV2Application implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("221-2121", "221-2122"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av. Matos", "105", "Sala 800", "Centro", "38220877", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
@@ -67,6 +86,11 @@ public class CursomcV2Application implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 	}
 	
 	
