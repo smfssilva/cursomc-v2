cafc3ad38425285b463322c8ce1e6b3f983a7a6d
smfssilva <smfssilva@yahoo.com>
2021-09-25 02:33:42 -0300

    Estado e Cidade

--- src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.smfssilva.cursomc.domain.Categoria;
+import com.smfssilva.cursomc.domain.Cidade;
+import com.smfssilva.cursomc.domain.Estado;
 import com.smfssilva.cursomc.domain.Produto;
 import com.smfssilva.cursomc.repositories.CategoriaRepository;
+import com.smfssilva.cursomc.repositories.CidadeRepository;
+import com.smfssilva.cursomc.repositories.EstadoRepository;
 import com.smfssilva.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcV2Application implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
@@ -41,9 +49,24 @@ public class CursomcV2Application implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 	
 	
