a1256b97bc7d162ca52a6c051631aa459e713c08
smfssilva <smfssilva@yahoo.com>
2021-09-24 23:29:19 -0300

    Projeto criado

---
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -0,0 +1,13 @@
+package com.smfssilva.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/smfssilva/cursomc/CursomcV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smfssilva.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
