305437888e3710b31c0754311bbb7b8cc921d30d
smfssilva <smfssilva@yahoo.com>
2021-09-25 17:55:50 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -61,6 +61,11 @@ public class CursomcV2Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônica");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
@@ -119,7 +124,7 @@ public class CursomcV2Application implements CommandLineRunner {
 		p3.getItens().addAll(Arrays.asList(ip2));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
