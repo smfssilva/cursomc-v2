164f211e78c4acf3a3131596dcb8664763fec2f6
smfssilva <smfssilva@yahoo.com>
2021-09-26 17:23:09 -0300

    Email HTML

--- src/main/java/com/smfssilva/cursomc/services/EmailService.java
+++ src/main/java/com/smfssilva/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.smfssilva.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.smfssilva.cursomc.domain.Pedido;
@@ -9,5 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 
 }
