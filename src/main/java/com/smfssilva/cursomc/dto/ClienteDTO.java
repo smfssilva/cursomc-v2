c8784e14e15b6423cd3fa843d44dc619bd967da5
smfssilva <smfssilva@yahoo.com>
2021-09-25 22:25:48 -0300

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/smfssilva/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/smfssilva/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.smfssilva.cursomc.domain.Cliente;
+import com.smfssilva.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
