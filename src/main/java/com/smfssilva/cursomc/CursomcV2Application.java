4b435eca07199a0b152194136319375129a8ade1
smfssilva <smfssilva@yahoo.com>
2021-09-27 02:01:51 -0300

    Enviando imagem via endpoint

--- src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -1,18 +1,13 @@
 package com.smfssilva.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.smfssilva.cursomc.services.S3Service;
 
 
 @SpringBootApplication
 public class CursomcV2Application implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
@@ -20,9 +15,6 @@ public class CursomcV2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		s3Service.uploadFile("/home/silva/Desktop/picture.jpeg");
-		
 	}
 
 }
