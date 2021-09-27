dfb41d53efbd9757ed87304ae6a4f674d895ab92
smfssilva <smfssilva@yahoo.com>
2021-09-26 22:24:32 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/com/smfssilva/cursomc/services/ClienteService.java
+++ src/main/java/com/smfssilva/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.smfssilva.cursomc.domain.Cidade;
 import com.smfssilva.cursomc.domain.Cliente;
 import com.smfssilva.cursomc.domain.Endereco;
+import com.smfssilva.cursomc.domain.enums.Perfil;
 import com.smfssilva.cursomc.domain.enums.TipoCliente;
 import com.smfssilva.cursomc.dto.ClienteDTO;
 import com.smfssilva.cursomc.dto.ClienteNewDTO;
 import com.smfssilva.cursomc.repositories.ClienteRepository;
 import com.smfssilva.cursomc.repositories.EnderecoRepository;
+import com.smfssilva.cursomc.security.UserSS;
+import com.smfssilva.cursomc.services.exceptions.AuthorizationException;
 import com.smfssilva.cursomc.services.exceptions.DataIntegretyException;
 import com.smfssilva.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
---
+++ src/main/java/com/smfssilva/cursomc/services/UserService.java
@@ -0,0 +1,17 @@
+package com.smfssilva.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.smfssilva.cursomc.security.UserSS;
+
+public class UserService {
+
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+}
