ec596e5fd90eaa7d1b2016dd8ff6bf0d4b24dd3e
smfssilva <smfssilva@yahoo.com>
2021-09-26 21:44:36 -0300

    Implementando autorização

--- src/main/java/com/smfssilva/cursomc/config/SecurityConfig.java
+++ src/main/java/com/smfssilva/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.smfssilva.cursomc.security.JWTAuthenticationFilter;
+import com.smfssilva.cursomc.security.JWTAuthorizationFilter;
 import com.smfssilva.cursomc.security.JWTUtil;
 
 @Configuration
@@ -62,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
--- src/main/java/com/smfssilva/cursomc/security/JWTUtil.java
+++ src/main/java/com/smfssilva/cursomc/security/JWTUtil.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 @Component
@@ -25,4 +25,32 @@ public class JWTUtil {
 				.compact();
 	}
 	
+	public boolean tokenValido(String token) {
+		Claims cleaims = getClaims(token);
+		if (cleaims != null) {
+			String username = cleaims.getSubject();
+			Date expirationDate = cleaims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String getUsername(String token) {
+		Claims cleaims = getClaims(token);
+		if (cleaims != null) {
+			return cleaims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
