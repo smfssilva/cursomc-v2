e1f8db75cd912361e34ea4f6c73ec363ddce603b
smfssilva <smfssilva@yahoo.com>
2021-09-27 01:16:28 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
+++ src/main/java/com/smfssilva/cursomc/CursomcV2Application.java
@@ -1,39 +1,18 @@
 package com.smfssilva.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.smfssilva.cursomc.domain.Categoria;
-import com.smfssilva.cursomc.domain.Cidade;
-import com.smfssilva.cursomc.domain.Cliente;
-import com.smfssilva.cursomc.domain.Endereco;
-import com.smfssilva.cursomc.domain.Estado;
-import com.smfssilva.cursomc.domain.ItemPedido;
-import com.smfssilva.cursomc.domain.Pagamento;
-import com.smfssilva.cursomc.domain.PagamentoComBoleto;
-import com.smfssilva.cursomc.domain.PagamentoComCartao;
-import com.smfssilva.cursomc.domain.Pedido;
-import com.smfssilva.cursomc.domain.Produto;
-import com.smfssilva.cursomc.domain.enums.EstadoPagamento;
-import com.smfssilva.cursomc.domain.enums.TipoCliente;
-import com.smfssilva.cursomc.repositories.CategoriaRepository;
-import com.smfssilva.cursomc.repositories.CidadeRepository;
-import com.smfssilva.cursomc.repositories.ClienteRepository;
-import com.smfssilva.cursomc.repositories.EnderecoRepository;
-import com.smfssilva.cursomc.repositories.EstadoRepository;
-import com.smfssilva.cursomc.repositories.ItemPedidoRepository;
-import com.smfssilva.cursomc.repositories.PagamentoRepository;
-import com.smfssilva.cursomc.repositories.PedidoRepository;
-import com.smfssilva.cursomc.repositories.ProdutoRepository;
+import com.smfssilva.cursomc.services.S3Service;
+
 
 @SpringBootApplication
 public class CursomcV2Application implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcV2Application.class, args);
@@ -41,6 +20,9 @@ public class CursomcV2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("/home/silva/Desktop/picture.jpeg");
+		
 	}
 
 }
